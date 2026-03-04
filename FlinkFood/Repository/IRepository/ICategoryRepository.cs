8c7802b18c9c5de44c7444ad0949f2f5eaf97d70
Fantomas077 <nheltonn@gmail.com>
2026-03-04 10:02:04 +0100

    AddCategoryTable and Seed The Table. also ICategoryRepository

---
+++ FlinkFood/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using FlinkFood.Data;
+
+namespace FlinkFood.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        Task AddAsync(Category category);
+        Task UpdateAsync(Category category);
+        Task DeleteAsync(Category category);
+        Task<Category?> FindById(int id);
+        Task<IEnumerable<Category>> GetAllAsync();
+    }
+}
